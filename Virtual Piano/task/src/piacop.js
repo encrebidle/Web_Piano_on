542e8239220e27830f6e89aa36039a95eb6642da
psychozonestartshere <psychozonestartshere@gmail.com>
2021-01-20 23:07:10 +0530

    done3

--- Virtual Piano/task/src/piacop.js
+++ Virtual Piano/task/src/piacop.js
@@ -1,79 +1,17 @@
-document.addEventListener("keydown", function (event) {
+let whiteKeys = ["KeyA", "KeyS", "KeyD", "KeyF", "KeyG", "KeyH", "KeyJ"]
+let blackKeys = ["KeyW", "KeyE", "KeyT", "KeyY", "KeyU"]
 
-    if (event.code == "KeyA") {
-        console.log("The 'A' key is pressed.");
-        let audio = document.createElement("AUDIO");
-        audio.src = "Audio/do.mp3";
-        audio.play();
-    }
-    else if (event.code == "KeyS") {
-        console.log("The 'S' key is pressed.");
-        let audio = document.createElement("AUDIO");
-        audio.src = "Audio/re.mp3";
-        audio.play();
-    }
-    else if (event.code == "KeyD") {
-        console.log("The 'D' key is pressed.");
-        let audio = document.createElement("AUDIO");
-        audio.src = "Audio/mi.mp3";
-        audio.play();
-    }
-    else if (event.code == "KeyF") {
-        console.log("The 'F' key is pressed.");
-        let audio = document.createElement("AUDIO");
-        audio.src = "Audio/fa.mp3";
-        audio.play();
-    }
-    else if (event.code == "KeyG") {
-        console.log("The 'G' key is pressed.");
-        let audio = document.createElement("AUDIO");
-        audio.src = "Audio/sol.mp3";
-        audio.play();
-    }
-    else if (event.code == "KeyH") {
-        console.log("The 'H' key is pressed.");
-        let audio = document.createElement("AUDIO");
-        audio.src = "Audio/lja.mp3";
-        audio.play();
-    }
-    else if (event.code == "KeyJ") {
-        console.log("The 'J' key is pressed.");
-        let audio = document.createElement("AUDIO");
-        audio.src = "Audio/si.mp3";
-        audio.play();
-    }
-    else if (event.code == "KeyW") {
-        console.log("The 'W' key is pressed.");
-        let audio = document.createElement("AUDIO");
-        audio.src = "Audio/do-diez.mp3";
-        audio.play();
-    }
-    else if (event.code == "KeyE") {
-        console.log("The 'E' key is pressed.");
-        let audio = document.createElement("AUDIO");
-        audio.src = "Audio/re-diez.mp3";
-        audio.play();
-    }
-    else if (event.code == "KeyT") {
-        console.log("The 'T' key is pressed.");
-        let audio = document.createElement("AUDIO");
-        audio.src = "Audio/fa-diez.mp3";
-        audio.play();
-    }
-    else if (event.code == "KeyY") {
-        console.log("The 'Y' key is pressed.");
-        let audio = document.createElement("AUDIO");
-        audio.src = "Audio/sol-diez.mp3";
-        audio.play();
-    }
-    else if (event.code == "KeyU") {
-        console.log("The 'U' key is pressed.");
-        let audio = document.createElement("AUDIO");
-        audio.src = "Audio/lja-diezi.mp3";
-        audio.play();
-    }
-    else {
-        console.log("Warning");
-        alert('This key is not handle')
-    }
+
+document.addEventListener("keydown", function (piano) {
+    if (whiteKeys.includes(piano.code)) {
+        let pianokey = new Audio("white_keys/" + piano.code.replace("Key", "") + ".mp3");
+        pianokey.play();
+        console.log("The '" + piano.code.replace("Key", "") + "' key is pressed");
+    } else if (blackKeys.includes(piano.code)) {
+        let pianokey = new Audio("black_keys/" + piano.code.replace("Key", "") + ".mp3");
+        pianokey.play();
+        console.log("The '" + piano.code.replace("key", "") + "' key is pressed");
+    } else console.log("The '" + piano.code.replace("Key", "") + "' key is not on the piano");
+    alert("This key is not a handle")
+
 });
